5a99a5064481b460e59d5d35de3513476ea81b18
Cocor <mac2222.ai@gmail.com>
2021-12-01 15:09:22 +0100

    Main Change

---
+++ src/com/cc/java/Cat.java
@@ -0,0 +1,12 @@
+package com.cc.java;
+
+public class Cat {
+
+    public static void main(String[] args) {
+        
+        Cat cat = new Cat();
+
+        System.out.println(cat);
+    }
+    
+}
