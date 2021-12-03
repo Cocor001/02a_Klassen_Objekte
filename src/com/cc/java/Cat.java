b3a258d87234051ee41c77eb43c5845aed069456
Cocor <mac2222.ai@gmail.com>
2021-12-03 08:41:44 +0100

    Main Code

--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -1,12 +1,39 @@
 package com.cc.java;
 
 public class Cat {
-
-    public static void main(String[] args) {
-        
-        Cat cat = new Cat();
-
-        System.out.println(cat);
-    }
     
+    private String name;
+    private String furColor;
+    private int age;
+
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    public Cat getAddress(){
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+       this.name = name; 
+    }
+
+    public String getFurColor() {
+        return furColor;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+  
+    
+
+
 }
